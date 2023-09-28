0edc2a3c3f8ed001edeafb2cd01704f04eed744b
Michael <michaellngriffin@gmail.com>
2023-09-28 03:50:26 -0400

    started tests

---
+++ productiv/src/_testCommon.js
@@ -0,0 +1,22 @@
+const TEST_TODOS = [
+  {
+    id: 1,
+    title: "Test1",
+    description: "Test1 - priority2",
+    priority: 2,
+  },
+  {
+    id: 2,
+    title: "Test2",
+    description: "Test2 -priority1",
+    priority: 1,
+  },
+  {
+    id: 3,
+    title: "Test3",
+    description: "Test3 - priority3",
+    priority: 3,
+  },
+];
+
+
